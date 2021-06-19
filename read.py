568d0652d18410931729e7791adcffc29897ab86
YID <yi19871230@hotmail.com>
2021-06-20 00:44:31 +0800

    New add fild

--- read.py
+++ read.py
@@ -12,3 +12,10 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('每則留言平均字數為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆留言小於100')
+print(new[0])		
