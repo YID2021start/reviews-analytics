ea681299102767de34e407c8900f4a38e4a1fe92
YID2021start <86122724+YID2021start@users.noreply.github.com>
2021-06-25 12:05:26 +0800

    Add files via upload

--- read.py
+++ read.py
@@ -8,6 +8,38 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了,總共有',len(data),'筆資料')
 
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每則留言平均字數為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆留言小於100')
+		
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+
+# 進階寫法
+# good = [d for d in data if 'good' in d]
+# print(len(good))
+
+
+bad = []
+for d in data:
+	bad.append('bad' in d)
+print(bad)
+# 進階寫法
+# bad = ['bad' in d for d in data]
+# print(len(bad))
+
+
 print(data[0])
 wc = {} #word_count #字典使用方式
 for d in data:
@@ -47,33 +79,3 @@ print('感謝使用本查詢功能')
 
 
 
-#sum_len = 0
-#for d in data:
-#	sum_len += len(d)
-#print('每則留言平均字數為', sum_len/len(data))
-
-#new = []
-#for d in data:
-#	if len(d) < 100:
-#		new.append(d)
-#print('一共有',len(new),'筆留言小於100')
-		
-
-#good = []
-#for d in data:
-#	if 'good' in d:
-#		good.append(d)
-
-#進階寫法
-#good = [d for d in data if 'good' in d]
-#print(len(good))
-
-
-#bad = []
-#for d in data:
-#	bad.append('bad' in d)
-#print(bad)	
-
-#進階寫法
-#bad = ['bad' in d for d in data]
-#print(len(bad))
